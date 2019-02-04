a73112b935c14d76637ca363cf155a2a5468353a
Martí Bosch <marti.bosch@epfl.ch>
2019-02-04 11:54:59 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='swiss_urbanization',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Materials necessary to reproduce the paper',
+    author='Martí Bosch',
+    license='GPL-3.0',
+)
