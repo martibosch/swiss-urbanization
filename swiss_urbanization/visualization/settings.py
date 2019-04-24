9470a32be0c181923ba8630d6dd075ca9e95a774
Martí Bosch <marti.bosch@epfl.ch>
2019-04-24 14:55:00 +0200

    metrics time series fig with metric abbrev as ylabel

---
+++ swiss_urbanization/visualization/settings.py
@@ -0,0 +1,12 @@
+metric_abbreviations = {
+    'proportion_of_landscape': 'PLAND',
+    'area_mn': 'MPS',
+    'largest_patch_index': 'LPI',
+    'patch_density': 'PD',
+    'edge_density': 'ED',
+    'fractal_dimension_am': 'AWMFD',
+    'landscape_shape_index': 'LSI',
+    'euclidean_nearest_neighbor_mn': 'ENN',
+    'contagion': 'CONTAG',
+    'shannon_diversity_index': 'SHDI'
+}
