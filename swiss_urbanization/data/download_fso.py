9876727c1208c1ca82ad4ebaa1c20e628e42c102
Martí Bosch <marti.bosch@epfl.ch>
2019-11-15 15:53:44 +0100

    updated environment.yml (only v nums), notebooks and code (pls v2)

--- swiss_urbanization/data/download_fso.py
+++ swiss_urbanization/data/download_fso.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 import logging
-from pathlib import Path
 from urllib import request
 
 import click
-from dotenv import find_dotenv, load_dotenv
 
 
 @click.command()
@@ -22,12 +20,5 @@ def main(uri, output_filepath):
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
     main()
--- swiss_urbanization/data/make_agglomeration_extract.py
+++ swiss_urbanization/data/make_agglomeration_extract.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 import logging
-from pathlib import Path
 
 import click
 import geopandas as gpd
 import swisslandstats as sls
-from dotenv import find_dotenv, load_dotenv
 from slugify import slugify
 
 from swiss_urbanization.data import settings
@@ -77,12 +75,5 @@ def main(sls_filepath, gmb_filepath, agglomeration_slug, extract_filepath):
 if __name__ == '__main__':
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
 
     main()
